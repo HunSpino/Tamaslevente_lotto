d5aa6eeebacc38998e0a8bc9ba115cb2b24c3c18
Tamas-Levente <tamasleventelaszlo99@gmail.com>
2021-12-01 16:33:34 +0100

    fx design megvan,cotroller.java megkezdve

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    private Button btnSorsolRendez;
+    @FXML
+    private Label sorszam;
+    @FXML
+    private ListView<String> sorszamLista;
+    private boolean running;
+
+
+
+
+    private void sorsolRendezClick() {
+        
+    }
 }
